55b9280e5b0c58ff7aa5a3003951fa48e2f49848
Abdelrhman <kabdo17013@gmail.com>
2024-03-10 21:21:27 +0200

    java streams in some details

--- src/main/java/streamsapi/Main.java
+++ src/main/java/streamsapi/Main.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         List<Person> people = getPeople();
 
         //Imperative approach
